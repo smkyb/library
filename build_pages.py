3ed24eb572537453190b1e13b414aeba7bd187fb
smkyb@users.noreply.github.com <minatospmyabe@gmail.com>
2025-08-14 18:47:04 +0900

    update build_pages.py

--- build_pages.py
+++ build_pages.py
@@ -6,32 +6,33 @@ os.makedirs("gh_pages")
 cnt_pages = 0
 
 def WriteTagU(f):
-    f.write("<!DOCTYPE html>\n" \
-    "<html lang=\"ja\">\n" \
-    "<head>\n" \
-    "<meta charset=\"UTF-8\">\n" \
-    "<title>smkyb's library</title>\n" \
-    "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap\">\n" \
-    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.1/github-markdown.min.css\"></script>\n" \
-    "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css\">\n" \
-    "<style>\n" \
-    "body { margin: 60px; font-family:'Noto Sans JP', Arial, sans-serif; font-size:large; }\n" \
-    ".markdown-body { box-sizing: border-box; max-width: 900px; margin: 0 auto; }\n" \
-    ".markdown-body pre { padding: 16px; overflow: auto; }\n" \
-    "</style>\n" \
-    "</head>\n" \
-    "<body>\n")
+    f.write("""<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>smkyb's library</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.1/github-markdown.min.css"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css">
+<style>
+    body { margin: 60px; font-family:'Noto Sans JP', Arial, sans-serif; font-size:large; }
+    .markdown-body { box-sizing: border-box; max-width: 900px; margin: 0 auto; }
+    .markdown-body pre { padding: 16px; overflow: auto; }
+</style>
+</head>
+<body>
+""")
 
 def WriteTagD(f):
-    f.write("<script src=\"https://cdn.jsdelivr.net/npm/marked/marked.min.js\"></script>" \
-    "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js\"></script>\n" \
-    "<script>\n" \
-    "    marked.setOptions({ gfm: true, breaks: true, headerIds: true, mangle: false, highlight: (code, lang) => { if (lang && hljs.getLanguage(lang)) { return hljs.highlight(code, { language: lang }).value; } return hljs.highlightAuto(code).value; } });\n" \
-    "    const el = document.getElementById('md_content');\n" \
-    "    el.innerHTML = marked.parse(el.textContent);\n"
-    "</script>\n"
-    "</body>\n" \
-    "</html>\n")
+    f.write("""<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<script>
+    marked.setOptions({ gfm: true, breaks: true, headerIds: true, mangle: false, highlight: (code, lang) => { if (lang && hljs.getLanguage(lang)) { return hljs.highlight(code, { language: lang }).value; } return hljs.highlightAuto(code).value; } });
+    const el = document.getElementById("md_content");
+    el.innerHTML = marked.parse(el.textContent);
+</script>
+</body>
+</html>""")
 
 def FindCppFiles(path:str) -> str:
     global cnt_pages
